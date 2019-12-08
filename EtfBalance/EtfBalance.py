a5aef2f8033a168c3ea0ee7dcaec0d4f2a4662fd
Giglagla <guillaume.gamb@gmail.com>
2019-12-08 14:31:37 +0100

    MVC 1st try

--- EtfBalance/EtfBalance.py
+++ EtfBalance/EtfBalance.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,\
-    QLabel, QTableWidget,QTableWidgetItem
 from copy import deepcopy
 import sys
 
 
 def pgcd(*n):
-    """Calcul du 'Plus Grand Commun Diviseur' de n (>=2) valeurs entières (Euclide)"""
+    """Calcul du 'Plus Grand Commun Diviseur' de n (>=2) valeurs entières"""
 
     def _pgcd(a, b):
         while b: a, b = b, a % b
@@ -27,138 +25,103 @@ class Etf:
         self.rate = rate
         self.wishedRatio = wishedRatio
         self.realRatio = 0
-        
+
     def __repr__(self):
-        return "<Etf label:'%s'\t number:%s\t rate:%s\t wishedRatio:%s\t realRatio:%s>" % (self.label, self.number, self.rate, self.wishedRatio, self.realRatio)
-        
+        return "<Etf label:'%s'\t number:%s\t rate:%s\t wishedRatio:%s\t realRatio:%s>" % \
+            (self.label, self.number, self.rate, self.wishedRatio, self.realRatio)
+
     def __eq__(self, other):
-            return (self.label == self.label
-                    and self.number == other.number
-                    and self.rate == other.rate 
-                    and self.wishedRatio == other.wishedRatio
-                    and self.realRatio == other.realRatio)
-            
+        return (self.label == self.label
+                and self.number == other.number
+                and self.rate == other.rate
+                and self.wishedRatio == other.wishedRatio
+                and self.realRatio == other.realRatio)
+
     def __hash__(self):
-        return hash((self.label, self.number, self.rate, self.wishedRatio, self.realRatio))
-    
+        return hash((self.label, self.number, self.rate, self.wishedRatio,
+                     self.realRatio))
+
     def __ne__(self, other):
         return not self == other
-        
+
     def value(self):
         return self.number * self.rate
-        
-        
+
+
 class Wallet:
-        
-    def __init__(self, etfList=[]):
-        self.etfList = etfList
+    def __init__(self, name):
+        self.name = name
+        self.etfList = [Etf('Amundi ETF MSCI Emg Markets', 1, 19.979, 0.2),
+                        Etf('BNP Easy S&P 500', 8, 13.3035, 0.6),
+                        Etf('Vanguard FTSE Developed Europe', 1, 31.988, 0.2)]
         self.computeEtfRatio()
-        
+
     def __repr__(self):
         return ('[%s]' % ',\n '.join(map(str, self.etfList)))
-    
+
     def __eq__(self, other):
-        return self.etfList == other.etfList
-        
-    def __ne__(self, other):         
+        return (self.etfList == other.etfList
+                and self.name == other.name)
+
+    def __ne__(self, other):
         return not self == other
-    
+
     def computeEtfRatio(self):
+        """ Compute each Etf ratio inside this wallet"""
         for elem in self.etfList:
-            elem.realRatio = round(elem.value() / self.total(), 2)
-            
-    def total(self):
+            elem.realRatio = round(elem.value() / self.totalAmount(), 2)
+
+    def totalAmount(self):
+        """ Compute the total wallet amount """
         total = 0
         for elem in self.etfList:
             total += elem.value()
         return total
 
-   
-class WalletManager:
+    def balance(self, ratioPrecision=0.9):
+        """ Balance the wallet with a given precision,
+        in order to reach the wished ratio for each etf"""
+        lastBalancedWallet = Wallet("")
+        walletToBalance = deepcopy(self)
+        walletToBalance.name = "Balanced wallet"
 
-    def __init__(self, originalWallet):
-        self.originalWallet = originalWallet
-        self.balancedWallet = originalWallet
- 
-    def balanceWallet(self, ratioPrecision=1): 
-        lastBalancedWallet = self.originalWallet
-        newBalancedWallet = deepcopy(self.originalWallet)
-    
-        while(lastBalancedWallet != newBalancedWallet):
-            lastBalancedWallet = deepcopy(newBalancedWallet)
-            
-            for elem in newBalancedWallet.etfList:
-                newBalancedWallet.computeEtfRatio()
+        while(lastBalancedWallet != walletToBalance):
+            for elem in walletToBalance.etfList:
+                walletToBalance.computeEtfRatio()
                 while (elem.realRatio < (elem.wishedRatio * ratioPrecision)):
                     elem.number += 1
-                    newBalancedWallet.computeEtfRatio()  
-           
-            #print("balancedWallet : \n%s" % newBalancedWallet)
-        self.reduceWallet(newBalancedWallet)
-        self.balancedWallet = newBalancedWallet
-        return self.balancedWallet
- 
-    def reduceWallet(self, walletToReduce):
-        test = []
-        for elem in walletToReduce.etfList:
-            test.append(elem.number)
-        
-        pgcdFounded = pgcd(*test)
-        
-        for elem in walletToReduce.etfList:
-            elem.number = int(elem.number / pgcdFounded)
-            
-        while ( walletToReduce.total() > self.originalWallet.total() ):
-            for elem in walletToReduce.etfList:
-                elem.number += pgcdFounded
-    
-  
-            
-if __name__ == '__main__':
-    etfList = []
-    etfList.append(Etf('Amundi ETF MSCI Emg Markets', 5, 4.05, 0.2))
-    etfList.append(Etf('BNP Easy S&P 500', 8, 11.22, 0.6))
-    #etfList.append(Etf('SPDR MSCI Europe Small Cp Value Weighted', 1, 32.70, 0.2))    
-    etfList.append(Etf('Vanguard FTSE Developed Europe', 2, 29.00, 0.2))
-    binckWallet = Wallet(etfList)
-    
-    print("Binck wallet:\n%s" % binckWallet)
-    print("Total Wallet: %s" % binckWallet.total())
-    
-    wm = WalletManager(binckWallet)
-    balancedWallet = wm.balanceWallet(0.8)
-    print("\nBalanced wallet:\n%s" % balancedWallet)
-    print("Total Wallet: %s" % balancedWallet.total())  
-    
-    
-    app = QApplication(sys.argv)
-    window = QWidget()
-    layout = QVBoxLayout()
-    
-    label1 = QLabel("Binck Wallet:")
-    
-    # Binck Wallet
-    tableWidget = QTableWidget()
-    tableWidget.setColumnCount(5)
-    tableWidget.setHorizontalHeaderLabels(['Label', 'Number', 'Rate', 'Wished Ratio', 'Real Ratio'])
-    for etf in binckWallet.etfList:
-        index = binckWallet.etfList.index(etf)
-        tableWidget.insertRow(index)
-        QTableWidgetItem()
-        tableWidget.setItem(index, 0, QTableWidgetItem(etf.label))
-        tableWidget.setItem(index, 1, QTableWidgetItem(str(etf.number)))
-        tableWidget.setItem(index, 2, QTableWidgetItem(str(etf.rate)))
-        tableWidget.setItem(index, 3, QTableWidgetItem(str(etf.wishedRatio)))
-        tableWidget.setItem(index, 4, QTableWidgetItem(str(etf.realRatio)))
-    tableWidget.resizeColumnsToContents()
-    tableWidget.resizeRowsToContents()
-    
-    layout.addWidget(label1)
-    layout.addWidget(tableWidget)
-    layout.addWidget(QPushButton('Bottom'))
-    
-    window.setLayout(layout)
+                    walletToBalance.computeEtfRatio()
+            lastBalancedWallet = deepcopy(walletToBalance)
 
-    window.show()
-    app.exec_()
-    
+        walletToBalance.reduce(self.totalAmount())
+        return walletToBalance
+
+    def reduce(self, minVal):
+        """ Try to reduce the wallet, keeping the etf ratio untouched """
+        etfNumbers = []
+        for elem in self.etfList:
+            etfNumbers.append(elem.number)
+
+        pgcdFound = pgcd(*etfNumbers)
+
+        """ Set Etf numbers to PGCD """
+        for elem in self.etfList:
+            elem.number = int(elem.number / pgcdFound)
+
+        """ Reduce Etf numbers """
+        while (self.totalAmount() < minVal):
+            for elem in self.etfList:
+                elem.number += pgcdFound
+
+
+if __name__ == '__main__':
+
+    binckWallet = Wallet("Binck wallet")
+    print("%s:\n%s" % (binckWallet.name, binckWallet))
+    print("Total Wallet: %s" % binckWallet.totalAmount())
+
+    balancedWallet = binckWallet.balance(0.9)
+    print("%s:\n%s" % (balancedWallet.name, balancedWallet))
+    print("Total Wallet: %s" % balancedWallet.totalAmount())
+
+    sys.exit()
--- EtfBalance/Gui.py
+++ EtfBalance/Gui.py
@@ -5,16 +5,53 @@ Created on 31 janv. 2019
 @author: guillaume
 '''
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,\
+    QLabel, QTableWidget, QTableWidgetItem, QAbstractScrollArea, QMainWindow
+from PyQt5 import QtCore
 import sys
+import EtfBalance
 
 
-class MainWindow():
-    def __init__(self):
-        self.nom=0
-            
+class View(object):
+
+    @staticmethod
+    def buildQTableWidget(wallet):
+        qtwWallet = QTableWidget()
+        qtwWallet.setColumnCount(5)
+        qtwWallet.setHorizontalHeaderLabels(['Label', 'Number', 'Rate',
+                                             'Wished Ratio', 'Real Ratio'])
+        for etf in wallet.etfList:
+            index = wallet.etfList.index(etf)
+            qtwWallet.insertRow(index)
+            QTableWidget()
+            qtwWallet.setItem(index, 0, QTableWidgetItem(etf.label))
+            qtwWallet.setItem(index, 1, QTableWidgetItem(str(etf.number)))
+            qtwWallet.setItem(index, 2, QTableWidgetItem(str(etf.rate)))
+            qtwWallet.setItem(index, 3, QTableWidgetItem(str(etf.wishedRatio)))
+            qtwWallet.setItem(index, 4, QTableWidgetItem(str(etf.realRatio)))
+
+        qtwWallet.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        qtwWallet.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        qtwWallet.resizeColumnsToContents()
+        qtwWallet.resizeRowsToContents()
+        qtwWallet.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        return qtwWallet
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setApplicationName("EtfBalance")
-    app.setStyle("Fusion")
+
+    binckWallet = EtfBalance.Wallet("Binck wallet")
+    balancedWallet = binckWallet.balance(0.9)
+
+    window = QWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(QLabel(binckWallet.name))
+    layout.addWidget(View.buildQTableWidget(binckWallet))
+    layout.addWidget(QLabel(balancedWallet.name))
+    layout.addWidget(View.buildQTableWidget(balancedWallet))
+    layout.addWidget(QPushButton('Balanced'))
+    window.setLayout(layout)
+
+    window.show()
     sys.exit(app.exec_())
