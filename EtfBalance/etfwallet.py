f997de3c6465c87d93328d6e4a925c1d9997c99d
Guillaume <guillaume.gamb@gmail.com>
2022-12-03 18:53:24 +0100

    Adding S&P 500 ETF

--- EtfBalance/etfwallet.py
+++ EtfBalance/etfwallet.py
@@ -76,11 +76,11 @@ class Wallet:
                                        'PTPXE.PA',
                                        'PAASI.PA',
                                        'RS2K.PA'))
-        self.etfList = [Etf('Amundi ETF PEA MSCI Emg Markets', 7, paaem, 0.12), # WW Emg
-                        Etf('BNP Easy S&P 500', 36, ese, 0.45),                 # US
+        self.etfList = [Etf('Amundi ETF PEA MSCI Emg Markets', 12, paaem, 0.12), # WW Emg
+                        Etf('BNP Easy S&P 500', 52, ese, 0.45),                 # US
                         Etf('BNP Paribas Easy Stoxx Europe 600', 25, etz, 0.1), # Europe
                         Etf('Amundi ETF PEA Japan TOPIX', 6, ptpxe, 0.08),       # Japan
-                        Etf('Amundi MSCI EM ASIA', 5, paasi, 0.1),              # Asia
+                        Etf('Amundi MSCI EM ASIA', 10, paasi, 0.1),              # Asia
                         Etf('Amundi ETF Russel 2000', 1, rs2k, 0.15)]           # US small
         self.computeEtfRatio()
 
